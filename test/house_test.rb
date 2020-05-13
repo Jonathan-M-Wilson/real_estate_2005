5828f02b1553fc37382a8e5301b82b9def54b8a5
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-05-13 09:16:47 -0600

    Add tests for room class

---
+++ test/house_test.rb
@@ -0,0 +1,32 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/room'
+require './lib/house'
+
+class HouseTest < Minitest::Test
+
+  def setup
+    @room = Room.new(:bedroom, 10, '13')
+    @room2 = Room.new(:living_room, 15, '12')
+    @house = House.new("$400000", "123 sugar lane")
+  end
+
+  def test_it_exists
+    assert_instance_of House, @house
+  end
+
+  def test_it_has_a_price
+
+    assert_equal "$400000", @house.price
+  end
+
+  def test_it_has_an_address
+
+    assert_equal "123 sugar lane", @house.address
+  end
+
+  def test_it_has_rooms
+
+    assert_equal [], @house.rooms
+  end
+end
